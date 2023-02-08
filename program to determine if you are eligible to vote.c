6fa16793c9a5531ccd853dbae263c320e8c7a64e
marywandia <122989738+marywandia@users.noreply.github.com>
2023-02-08 10:53:17 +0300

    Add files via upload

---
+++ program to determine if you are eligible to vote.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int age;
+    printf("Enter the age of the person");
+    scanf("%d", &age);
+    if(age>=18);
+    {
+    printf("u r  very eligible");
+    }
+   if(age<=18);
+    {
+        printf("u r  eligible");
+    } 
+    return 0;
+}
+    
