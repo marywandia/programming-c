eaa9e0955b4400b5ec7566122479f365d7bc3bb8
marywandia <122989738+marywandia@users.noreply.github.com>
2023-02-22 21:13:13 +0300

    Add files via upload

---
+++ 50 natural numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    int n;
+    int sum=1+2+3+4+5+6+7+8+9+10;
+    for (n>=0; n<=10; n++){
+    printf("%d\n",n);
+    }
+    printf("the sum is %d\n,sum ");
+return 0;
+}
